5f7b97215d65dab1dcdbd74de2efd935d38f1d14
Madhvendra Singh <madhvendraofficial@gmail.com>
2023-04-18 12:38:20 +0530

    added sitemap.xml

---
+++ nextjs.config.js
@@ -0,0 +1,32 @@
+// import { configureSitemap } from '@sergeymyssak/nextjs-sitemap';
+const { configureSitemap } = require('@sergeymyssak/nextjs-sitemap');
+
+async function fetchDynamicPaths() {
+  return ['house', 'flower', 'table'];
+}
+
+async function getDynamicPaths() {
+  const paths = await fetchDynamicPaths();
+
+  return paths.map((item) => `/project/${item}`);
+}
+
+getDynamicPaths().then((paths) => {
+  const Sitemap = configureSitemap({
+    domains: [{ domain: 'animemadara.vercel.app', defaultLocale: 'en' }],
+    include: paths,
+    exclude: ['/project/*'],
+    excludeIndex: true,
+    pagesConfig: {
+      '': {
+        priority: '0.5',
+        changefreq: 'daily',
+      },
+    },
+    trailingSlash: true,
+    targetDirectory: __dirname + '/public',
+    pagesDirectory: __dirname + '/src/pages',
+  });
+
+  Sitemap.generateSitemap();
+});
